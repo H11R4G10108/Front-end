769419acb1cee0a3d2d7650fee3bed00daf02e1f
Nguyễn Lưu Phương Uyên <luuphuonguyen04@gmail.com>
2024-06-05 13:46:28 +0700

    empty

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Productpage from "./components/Productpage/Productpage";
+
 import { Button } from "@material-tailwind/react";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Productpage />
     </div>
   );
 }
--- src/components/ProductPage/Productpage.jsx
+++ src/components/ProductPage/Productpage.jsx
@@ -1,3 +1,29 @@
-function Productpage() {
-  return;
-}
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ProductService from "../../services/ProductService";
+
+const ProductList = () => {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    retrieveProduct();
+  }, []);
+
+  const retrieveProduct = () => {
+    ProductService.getAll()
+      .then((response) => {
+        setProduct(response.data);
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Product Page Test</h1>
+    </div>
+  );
+};
+export default ProductList;
