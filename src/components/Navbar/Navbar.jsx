31654c1be85492d00bf16a3382f21583f36e0d2d
Nguyễn Lưu Phương Uyên <luuphuonguyen04@gmail.com>
2024-06-06 16:52:04 +0700

    Router update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,11 @@ import {
   UserIcon,
   MagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
+import Productpage from "../../components/ProductPage/Productpage";
 import logo from "../../assets/logo.png";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { Routes, Route } from "react-router";
 
 export default function navbar() {
   return (
@@ -14,14 +16,14 @@ export default function navbar() {
         <img src={logo} alt="american apparel" className="w-32" />
       </a>
       <div className="flex gap-20">
-        <a
-          href="/product"
+        <Link
+          to={"/"}
           className="group transition-all duration-300 ease-in-out"
         >
           <p className="bg-left-bottom bg-gradient-to-r from-blue-500 to-pink-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out text-sm">
             Home
           </p>
-        </a>
+        </Link>
         <a href="#" className="group transition-all duration-300 ease-in-out">
           <p className="bg-left-bottom bg-gradient-to-r from-blue-500 to-pink-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out text-sm">
             For Men
@@ -40,15 +42,19 @@ export default function navbar() {
       </div>
       <div className="flex gap-10">
         <a href="#">
-          <MagnifyingGlassIcon className="h-6 w-6 text-black-0" href="#" />
+          <MagnifyingGlassIcon className="h-5 w-5 text-black-0" href="#" />
         </a>
         <a href="#">
-          <ShoppingCartIcon className="h-6 w-6 text-black-0" href="#" />
+          <ShoppingCartIcon className="h-5 w-5 text-black-0" href="#" />
         </a>
         <a href="#">
-          <UserIcon className="h-6 w-6 text-black-0" href="#" />
+          <UserIcon className="h-5 w-5 text-black-0" href="#" />
         </a>
       </div>
+
+      <Routes>
+        <Route path="/" element={<Productpage />} />
+      </Routes>
     </div>
   );
 }
